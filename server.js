bb21eebed279321d3689a44da91d334409057f6a
baileyjas4 <baileyjasmine4@icloud.com>
2026-01-16 14:25:44 -0500

    added comments to server.js file outliing whats important

---
+++ server.js
@@ -0,0 +1,25 @@
+//DEPENDECIES 
+
+//DATABASE CONNECTION 
+
+//DATABASE CONNECTION error/succes
+//Define callback function for various events
+
+//MIDDLEWARE
+
+//ROUTES- INDUCES
+
+
+//INDEX
+
+
+//NEW 
+
+//D
+//U
+//CREATE 
+
+//E
+//S
+
+//PORT
